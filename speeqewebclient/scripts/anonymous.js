869aa79c604264ae743b6d498a6dd0aa9f4e9dd0
Christopher Zorn <tofu@thetofu.com>
2008-10-24 19:38:26 +0000

    Merging source:branches/nick-function-local-32 fixes #32

--- speeqewebclient/scripts/anonymous.js
+++ speeqewebclient/scripts/anonymous.js
@@ -55,7 +55,7 @@ Speeqe.generate_anonymous_nick = function() {
     //pick a nice name for anonymous users
 
     var nick = Speeqe.NAMES[Speeqe.random(Speeqe.NAMES.length)-1];
-    var random_num = Speeqe.random(1000);
+    var random_num = Speeqe.random(65535);
     var ret_array = [nick,random_num];
     
     return ret_array.join("");
